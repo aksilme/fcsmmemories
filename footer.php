30505cda94ae7a0c72d2eb32bd546c21883a3d74
koceila <lounas.koceila@gmail.com>
2017-03-28 21:56:11 +0200

    initial commit

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+	
+	<?php wp_footer(); ?>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/vue.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/vue-resources.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/vue-router.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/app.js"></script>
+</body>
+</html>
